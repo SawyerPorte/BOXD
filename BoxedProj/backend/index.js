5a17604df201000477f9d95d02ff8ed809abccde
SawyerPorte <sporte@ucsc.edu>
2025-10-06 17:10:23 -0700

    app

--- BoxedProj/backend/index.js
+++ BoxedProj/backend/index.js
@@ -10,8 +10,12 @@ app.use(cors({
     origin: "https://boxd-5hg1.onrender.com"  // or specific URL for production
 }));
 
-// Serve frontend files
 app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Serve index.html on root
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // ---- WORD GENERATION LOGIC ----
 //let words = JSON.parse(fs.readFileSync('./words.json', 'utf8')).map(w => w.toUpperCase());
