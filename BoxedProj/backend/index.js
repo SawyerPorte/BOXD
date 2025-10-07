25c3c11eabf959bd7d33d12a59810456d26d5f99
SawyerPorte <sporte@ucsc.edu>
2025-10-06 17:13:31 -0700

    dash

--- BoxedProj/backend/index.js
+++ BoxedProj/backend/index.js
@@ -13,7 +13,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Serve index.html on root
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
