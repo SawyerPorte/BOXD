48353047c735fd09864dba9805439d29389d38b2
SawyerPorte <sporte@ucsc.edu>
2025-10-06 20:42:26 -0700

    index

--- BoxedProj/backend/index.js
+++ BoxedProj/backend/index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: "https://boxd-5hg1.onrender.com"  // or specific URL for production
 }));
 
-const frontendPath = path.join(__dirname, '../frontend');
+const frontendPath = path.join(__dirname, '../frontend/index.html');
 app.use(express.static(frontendPath));
 
 // Catch-all for SPA routing
